1ea358ef9b1903a1010fe62e5a73e40c313e0e01
Jean F. St-Louis <stlouis.jeanfrancois@sepaq.com>
2022-03-07 15:08:53 -0500

    final

--- src/components/UI/BaseButton.vue
+++ src/components/UI/BaseButton.vue
@@ -19,16 +19,19 @@ button {
   color: white;
   cursor: pointer;
 }
+
 button:hover,
 button:active {
   background-color: #270041;
   border-color: #270041;
 }
+
 .flat {
   background-color: transparent;
   color: #3a0061;
   border: none;
 }
+
 .flat:hover,
 .flat:active {
   background-color: #edd2ff;
